1691830e6de35e3fcd34051d7643462dd314c178
Gustavo Machado <gdmachado@me.com>
2015-02-25 11:28:26 -0300

    bump to version 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ history = open('CHANGELOG.md').read()
 
 setup(
     name = 'quantumpy',
-    version = '0.1.3',
+    version = '0.1.4',
     description = 'An ultra simple wrapper for the Socialmetrix Quantum API, with basic functionality',
     author = 'Gustavo Machado',
     author_email = 'gdmachado@me.com',
