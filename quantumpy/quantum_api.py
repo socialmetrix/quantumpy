3023e43796f18ac6b7935101765ae1f1f2e155d8
Gustavo Machado <gdmachado@me.com>
2015-03-19 18:11:55 -0300

    added paging functionality, added two twitter endpoints

--- quantumpy/quantum_api.py
+++ quantumpy/quantum_api.py
@@ -7,6 +7,7 @@ import six
 
 from quantumpy.exceptions import *
 from decimal import Decimal
+from urlparse import urlparse, parse_qsl
 
 class QuantumAPI(object):
     def __init__(self, account_id, jwt, url='https://api.quantum.socialmetrix.com/v1', timeout=None):
@@ -71,7 +72,7 @@ class QuantumAPI(object):
 
         return response
 
-    def get_facebook_profiles_posts(self, project_id, fanpage_id, since, until, ids, owner=None, type=None, timezone='UTC', retry=3):
+    def get_facebook_profiles_posts(self, project_id, fanpage_id, since, until, ids, owner=None, type=None, page=False, timezone='UTC', retry=3):
         """
         /accounts/{account_id}/projects/{project_id}/facebook/profiles/{fanpage_id}/posts?
             since={start_date}
@@ -88,7 +89,8 @@ class QuantumAPI(object):
             method = 'GET',
             path   = '/accounts/{}/projects/{}/facebook/profiles/{}/posts'.format(self.account_id, project_id, fanpage_id),
             params = params,
-            retry  = retry
+            retry  = retry,
+            page   = page
         )
 
         if response is False:
@@ -253,25 +255,86 @@ class QuantumAPI(object):
 
         return response
 
-    def _query(self, method, path, params=None, retry=0):
+    def get_twitter_profiles_tweets(self, project_id, profile_id, since, until, ids, owner=None, type=None, page=False, timezone='UTC', retry=3):
+        """
+        /accounts/{account_id}/projects/{project_id}/facebook/profiles/{profile_id}/tweets?
+            since={start_date}
+            until={end_date}
+            ids={profiles}
+            owner={owner}
+            type={type}
+            timezone={timezone}
+        Get all tweets for a given profile and period
+        """
+        args = locals()
+        params = {param: args[param] for param in ['since', 'until', 'ids', 'owner', 'type', 'timezone']}
+        response = self._query(
+            method = 'GET',
+            path   = '/accounts/{}/projects/{}/twitter/profiles/{}/tweets'.format(self.account_id, project_id, profile_id),
+            params = params,
+            retry  = retry,
+            page   = page
+        )
+
+        if response is False:
+            raise QuantumError('Could not get tweets for profile {}.'.format(profile_id))
+
+        return response
+
+    def get_twitter_profiles_tweet_interactions(self, project_id, since, until, ids, timezone='UTC', retry=3):
+        """
+        /accounts/{account_id}/projects/{project_id}/twitter/profiles/tweet-interactions/count/date?
+            since={start_date}
+            until={end_date}
+            ids={posts}
+            timezone={timezone}
+        Get post interactions for posts within a project
+        """
+        args = locals()
+        params = {param: args[param] for param in ['since', 'until', 'ids', 'timezone']}
+        response = self._query(
+            method = 'GET',
+            path   = '/accounts/{}/projects/{}/twitter/profiles/tweet-interactions/count/date'.format(self.account_id, project_id),
+            params = params,
+            retry  = retry
+        )
+
+        if response is False:
+            raise QuantumError('Could not get tweet interactions for project {}.'.format(project_id))
+
+        return response
+
+    def _query(self, method, path, params=None, retry=0, page=False):
         if not path.startswith('/'):
             if six.PY2:
                 path = '/' + six.text_type(path.decode('utf-8'))
             else:
                 path = '/' + path
 
-        url = self.url + path
         params = {param: params[param] for param in params if params[param] is not None} if params is not None else None
 
         try:
-            return self._request(method, url, params)
+            if page:
+                return self._paginate(method, path, params)
+            else:
+                return self._request(method, path, params)[0]
         except QuantumPythonError:
             if retry:
                 return self._query(method, path, params, retry - 1)
             else:
                 raise
 
-    def _request(self, method, url, params):
+    def _paginate(self, method, path, params):
+        while path:
+            if urlparse(path)[4] != '':
+                params = dict(parse_qsl(urlparse(path)[4]))
+                path = urlparse(path)[2]
+
+            result, path = self._request(method, path, params)
+
+            yield result
+
+    def _request(self, method, path, params):
         if params:
             for key in params:
                 value = params[key]
@@ -282,7 +345,7 @@ class QuantumAPI(object):
             if method == 'GET':
                 response = self.session.request(
                     method,
-                    url,
+                    self.url + path,
                     params          = params,
                     allow_redirects = True,
                     timeout         = self.timeout,
@@ -295,7 +358,14 @@ class QuantumAPI(object):
         except requests.RequestException as e:
             raise HTTPError(e)
 
-        return self._parse(response.content)
+        result = self._parse(response.content)
+
+        try:
+            next_url = result['paging']['next']
+        except(KeyError, TypeError):
+            next_url = None
+
+        return result, next_url
 
     def _parse(self, data):
         if type(data) == type(bytes()):
